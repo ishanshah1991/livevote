84e95efdc6d824bb1a9c18d41c62dd7e33f312ab
Ishan Shah <ishan.shah@brainvire.com@BVIAHMLAP-568.brainvire.com>
2026-04-25 18:12:42 +0530

    feat: register public poll routes (show, results, vote)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\Public\PollController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/polls/{poll}', [PollController::class, 'show'])->name('poll.show');
+Route::get('/polls/{poll}/results', [PollController::class, 'results'])->name('poll.results');
+Route::post('/polls/{poll}/vote', [PollController::class, 'vote'])->name('poll.vote');
